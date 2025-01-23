bbcf3825dd8bbf67b4112e1814df47193ac70d47
vaibhav3898 <vainhav3898@gmail.com>
2025-01-23 14:50:58 +0530

    practice of javaScript

---
+++ dompractice1.js
@@ -0,0 +1,2 @@
+import { greet } from "./domPractice.js";
+console.log(greet("vaibhav"));
---
+++ example.js
@@ -0,0 +1,7 @@
+
+import { multiply } from './multiplygun.js';
+
+console.log(multiply(3, 4)); // Outputs: 12
+
+import { add } from './multiplygun.js';
+console.log(add(6,7));
